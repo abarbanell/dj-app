4c9b2d6087b9a18ed23889373f87fbbb039dc285
Tobias Abarbanell <tobias@abarbanell.de>
2018-02-27 20:42:42 +0100

    OPS-50 - remove statsd from page cycle

--- app/app/views.py
+++ app/app/views.py
@@ -1,21 +1,7 @@
 from django.http import HttpResponse, Http404
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
-import statsd
-import os
-import psutil
-
-# TODO: get IP and prefix from environment
 
 def index(request):
-  StatsdHost = os.environ['STATSD_HOST']
-  StatsdPref = os.environ['STATSD_PREFIX']
-  c = statsd.StatsClient(StatsdHost, 8125, prefix=StatsdPref)
-  # cpu = psutil.cpu_percent(interval=1)
-  usage = psutil.disk_usage("/")
-  mem = psutil.virtual_memory()
-  # c.incr("route.index")
-  # c.gauge('cpu.percent', cpu)
-  # c.gauge('mem.percent', mem.percent)
   return render(request, 'app/index.html')
 
