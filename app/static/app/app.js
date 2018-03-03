8363e78f7e0773690ca2bce85cc8a9618ccab225
Tobias Abarbanell <tobias@abarbanell.de>
2018-03-03 17:50:55 +0100

    OPS-55 GET questions works in vue.js

--- app/static/app/app.js
+++ app/static/app/app.js
@@ -2,9 +2,25 @@
 
     var app = new Vue({
       el: '#app',
+      mounted() {
+        this.fetchData();
+      },
+      methods: {
+        fetchData() {
+          let t = this; // this will not be in scope in the callback
+          fetch('api/questions/').then(function(res) {
+            return res.json()
+          }).then(function(data) {
+            t.questions = data;
+            t.message = "API data fetched"
+          }).catch(function(err) {
+            console.error(err);
+          })
+        }
+      },
       data: {
         message: 'static data initialized', 
         title: 'Question list',
-        questions: [ { text: "q1" }, { text: "q2"} ]
+        questions: []
       }
     })
