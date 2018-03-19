823c340fe87230fcbc0af34402c1390bf26d01f5
Tobias Abarbanell <tobias@abarbanell.de>
2018-03-19 16:18:40 +0100

    master/detail components

--- app/static/app/app.js
+++ app/static/app/app.js
@@ -1,49 +1,112 @@
 
+Vue.component('question-detail', {
+  props: {
+    id: {
+      required: true
+    },
+    pub_date: { 
+      required: true 
+    },
+    question_text: { 
+      required: true
+    }
+  },
+  data() {
+    return {
+      isExpanded: false,
+      q: {
+        id: this.id, 
+        pub_date: this.pub_date,
+        question_text: this.question_text
+      }
+    };
+  },
+  template: `
+      <li class="list-group-item">
+        {{ q.question_text }}
+        <b-button variant="outline-secondary" size="sm" @click="toggle">toggle details view</b-button>
+        <div v-show="isExpanded"> 
+          <p>Expanded details go here.</p>
+          <input v-model="q.question_text" @blur="update"></input>
+        </div>
+      </li>
+    `,
+    methods: {
+      toggle() {
+        console.log("expand toggled");
+        this.isExpanded = !this.isExpanded
+      },
+      update() {
+        console.log("blur - save changes: " + this.qtext);
+        this.$emit("input", this.q);
+      }
+    }
+});
 
 Vue.component('question-list', {
-  template: '<ul><slot></slot></ul>'
+  template: `
+    <div>
+      <div class="card">
+        <ul class="list-group list-group-flush">
+          <question-detail  
+            v-for="question in questions" 
+            v-bind="question"
+            @input="updateQuestion">
+          </question-detail>
+        </ul>
+      </div>
+      <b-alert variant="secondary" show>{{ message }} </b-alert>
+    </div>
 
+  `,
+  data() {
+    return {
+      questions: [],
+      message: "question-list component initializing..."
+    }
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      let t = this; // this will not be in scope in the callback
+      fetch('api/questions/').then(function(res) {
+        return res.json()
+      }).then(function(data) {
+        t.questions = data;
+        t.message = "API data retrieved in component";
+      }).catch(function(err) {
+        console.error(err);
+      })
+    },
+    updateQuestion(q) {
+      let now = new Date();
+      q.pub_date = now.toISOString();
+      fetch('api/questions/' + q.id + '/', {
+        method: 'PUT', // or 'PUT'
+        body: JSON.stringify(q), 
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        })
+      }).then(function(data) {
+        console.log("---PUT---")
+        console.log(data);
+      }).catch(function(err) {
+        console.error("***PUT-ERROR****")
+        console.error(err);
+      })
+    }
+  }
 });
 
-Vue.component('question-detail', {
-  template: '<li><slot></slot></li>'
-});
+
 
 
     var app = new Vue({
       el: '#app',
-      mounted() {
-        this.fetchData();
-      },
       methods: {
-        fetchData() {
-          let t = this; // this will not be in scope in the callback
-          fetch('api/questions/').then(function(res) {
-            return res.json()
-          }).then(function(data) {
-            t.questions = data;
-            t.message = "API data retrieved";
-          }).catch(function(err) {
-            console.error(err);
-          })
-        },
-        updateQuestion(q) {
-          let now = new Date();
-          q.pub_date = now.toISOString();
-          fetch('api/questions/' + q.id + '/', {
-            method: 'PUT', // or 'PUT'
-            body: JSON.stringify(q), 
-            headers: new Headers({
-              'Content-Type': 'application/json'
-            })
-          }).then(function(data) {
-            console.log("---PUT---")
-            console.log(data);
-          }).catch(function(err) {
-            console.error("***PUT-ERROR****")
-            console.error(err);
-          })
-        },
+
         saveQuestion(q) {
           console.log(q);
           let now = new Date();
